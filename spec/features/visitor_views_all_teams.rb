d975821f5cccc1a2c0f1e62539f96df1d182c1ae
John <s.johnwatson@gmail.com>
2014-01-20 14:30:11 -0500

    complete team index page w/o styling

---
+++ spec/features/visitor_views_all_teams.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+feature 'public views all teams' do
+  scenario 'visit team index page' do
+    college = FactoryGirl.create(:college)
+    team_1 = FactoryGirl.create(:team, college: college)
+    team_2 = FactoryGirl.create(:team, college: college)
+    visit teams_path
+
+    expect(page).to have_content(team_1.team_name)
+    expect(page).to have_content(team_2.team_name)
+    save_and_open_page
+  end
+
+end
